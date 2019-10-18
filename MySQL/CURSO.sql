02e77da9cd92f86d897dfe61dbd3982eb9d82569
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2019-10-18 16:59:38 -0500

    MySQL

---
+++ MySQL/CURSO.sql
@@ -0,0 +1,13 @@
+USE venta;
+INSERT INTO CURSO
+(CODCUR, DESCUR, CATCUR, DURCUR)
+VALUES
+("JAV", "curso de programacion", "BUILD", 50),
+("PYTHON", "curso de Informatica", "BUILD", 100),
+("SCIENCES", "curso de Ciencia", "Experiment", 70),
+("HISTORY", "curso de Historia", "investigation", 80),
+("MATH", "curso de Logica", "REASON", 50);
+
+SELECT * FROM CURSO;
+
+
